6da974d47d4c1a13d173ed00a6ad22d197db1f0e
Jakub Nowosad <tupiszakaczki@gmail.com>
2022-04-04 12:10:10 +0200

    adds major improvements to the code

--- R/spatial_ising.R
+++ R/spatial_ising.R
@@ -1,38 +1,70 @@
-#' Title
+#' Ising model for spatial data
 #'
-#' @param x
-#' @param B
-#' @param J
-#' @param iter
+#' Performs simulations based on the given parameters of the Ising model
 #'
-#' @return
+#' @param x SpatRaster or matrix containing two values: -1 and 1
+#' @param B External pressure
+#' @param J Peer pressure - it regulates a degree of local harmonization
+#' @param timesteps Specifies how many iterations are performed on the input object.
+#' The output of this function has as many layers as the `timesteps` value.
+#' @param updates Specifies how many iterations are performed on the input object.
+#' By default it equals to the number of values in the input object.
+#' @param version By default, `1`, the `x` object is converted into a matrix
+#' (fast, but can be memory consuming); `version = 2` has a lower RAM impact, but
+#' is much slower
+#'
+#' @references Ising, E., 1924. Beitrag zur theorie des ferro-und paramagnetismus. Ph.D. thesis, Grefe & Tiedemann.
+#' @references Onsager, L., 1944. Crystal statistics. I. A two-dimensional model with an order-disorder transition. Physical Review 65 (3-4), 117.
+#' @references Brush, S. G., 1967. History of the Lenz-Ising model. Reviews of modern physics 39 (4), 883.
+#' @references Cipra, B. A., 1987. An introduction to the Ising model. The American Mathematical Monthly 94 (10), 937–959.
+#'
+#' @return Object of the same class as `x` with the number of layers specified by `timesteps`
 #' @export
 #'
 #' @examples
-spatial_ising = function(x, B, J, iter){
-  if (!inherits(x, "matrix")){
-    input_matrix = terra::as.matrix(x, wide = TRUE)
-    x_ext = terra::ext(x)
-    x_crs = terra::crs(x)
-  } else {
-    input_matrix = x
+#' data(r_start, package = "spatialising")
+#' ts1 = spatial_ising(r_start, B = -0.3, J = 0.7)
+#' ts10 = spatial_ising(r_start, B = -0.3, J = 0.7, timesteps = 10)
+spatial_ising = function(x, B, J, timesteps = 1, updates, version = 1){
+  if (timesteps > 1){
+    y = vector(mode = "list", length = timesteps)
+    y[[1]] = x
+    for (i in seq_len(timesteps)[-1]){
+      y[[i]] = spatial_ising(y[[i - 1]], B, J, timesteps = 1, updates, version)
+    }
+    if (version == 1){
+      is_output_not_matrix = !inherits(x, "matrix")
+      if (is_output_not_matrix){
+        x = terra::rast(y)
+      } else {
+        x = simplify2array(y)
+      }
+    }
+  } else if (timesteps == 1){
+    if (version == 1){
+      is_output_not_matrix = !inherits(x, "matrix")
+      if (is_output_not_matrix){
+        x_ext = terra::ext(x)
+        x_crs = terra::crs(x)
+        x = terra::as.matrix(x, wide = TRUE)
+      }
+    }
+    n_rows = nrow(x); n_cols = ncol(x)
+    if (missing(updates)){
+      updates = n_rows * n_cols
+    }
+    rxs = round(stats::runif(updates, min = 1, max = n_rows))
+    rys = round(stats::runif(updates, min = 1, max = n_cols))
+    for (i in seq_len(updates)){
+      x = single_flip2(x, B, J, rxs[i], rys[i], n_rows, n_cols)
+    }
+    if (version == 1){
+      if (is_output_not_matrix){
+        x = terra::rast(x, crs = x_crs, extent = x_ext)
+      }
+    }
   }
-    n_rows = nrow(input_matrix)
-    n_cols = ncol(input_matrix)
-  if (missing(iter)){
-    iter = n_rows * n_cols
-  }
-  rxs = round(stats::runif(iter, min = 1, max = n_rows))
-  rys = round(stats::runif(iter, min = 1, max = n_cols))
-  for (i in seq_len(iter)){
-    input_matrix = single_flip2(input_matrix, B, J, rxs[i], rys[i], n_rows, n_cols)
-  }
-  if (!inherits(x, "matrix")){
-    result = terra::rast(input_matrix, crs = x_crs, extent = x_ext)
-  } else {
-    result = input_matrix
-  }
-  return(result)
+  return(x)
 }
 energy_diff = function(focal, neigh, B, J){
   if (focal == 1){
@@ -79,8 +111,6 @@ single_flip = function(input_matrix, B, J) {
   return(input_matrix)
 }
 single_flip2 = function(input_matrix, B, J, rx, ry, n_rows, n_cols) {
-  # n_rows = nrow(input_matrix)
-  # n_cols = ncol(input_matrix)
   # choose random spin
   if (missing(rx)){
     rx = round(stats::runif(1, min = 1, max = n_rows))
