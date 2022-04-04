7e0bb16f75b12af1d083982615bab92e1663a702
Jakub Nowosad <tupiszakaczki@gmail.com>
2022-04-04 12:09:38 +0200

    adds test data

---
+++ R/data.R
@@ -0,0 +1,23 @@
+#' An example binary raster
+#'
+#' A raster file covering an area of 50x50 cells. The raster file contains two values: -1 and 1.
+#' `system.file("raster/r_start.tif", package = "spatialising")`
+#'
+#' @format A raster file
+#' @name r_start.tif
+NULL
+
+#' An example matrix object
+#'
+#' A matrix has 50 columns and 50 rows. The matrix contains two values: -1 and 1.
+#'
+#' @format A matrix
+"r_start"
+
+#' An example matrix object
+#'
+#' A matrix has 50 columns and 50 rows. The matrix contains two values: -1 and 1.
+#' This object was created with `r_end = spatial_ising(r_start, B = -0.3, J = 0.7)`
+#'
+#' @format A matrix
+"r_end"
---
+++ data-raw/prep-example-data.R
@@ -0,0 +1,19 @@
+RandomFieldsUtils::RFoptions(install = "no")
+library(terra)
+library(NLMR)
+library(landscapetools)
+set.seed(2022-03-23)
+r1a = NLMR::nlm_fbm(50, 50, fract_dim = 1)
+r2a = landscapetools::util_classify(r1a, weighting = c(0.95, 0.05))
+rcl = matrix(c(1, 2, -1, 1), ncol = 2)
+r_start = rast(r2a) * -1 + 3
+r_start = classify(r_start, rcl)
+plot(r_start)
+writeRaster(r_start, "inst/raster/r_start.tif", gdal = "of=COG", overwrite = TRUE)
+r_start = as.matrix(r_start, wide = TRUE)
+usethis::use_data(r_start)
+
+set.seed(2022-04-04)
+r_end = spatial_ising(r_start, B = -0.3, J = 0.7)
+usethis::use_data(r_end)
+
