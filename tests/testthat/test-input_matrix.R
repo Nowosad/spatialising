277e34709bf391a3402d111820fa1283e2e92e0f
Jakub Nowosad <tupiszakaczki@gmail.com>
2022-04-04 12:10:27 +0200

    adds tests

---
+++ tests/testthat/test-input_matrix.R
@@ -0,0 +1,11 @@
+set.seed(2022-04-04)
+data(r_start, package = "spatialising")
+data(r_end, package = "spatialising")
+ts1 = spatial_ising(r_start, B = -0.3, J = 0.7)
+ts10 = spatial_ising(r_start, B = -0.3, J = 0.7, timesteps = 10)
+
+test_that("calculations on matrices works", {
+  expect_equal(dim(ts1), c(50, 50))
+  expect_equal(dim(ts10), c(50, 50, 10))
+  expect_equal(ts1, r_end)
+})
