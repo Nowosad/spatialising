df8ad2bdae00a5b791c792a1959c7b9ab931f775
Jakub Nowosad <tupiszakaczki@gmail.com>
2022-12-02 10:13:06 +0100

    adds tests for metrics

---
+++ tests/testthat/test-metrics.R
@@ -0,0 +1,21 @@
+set.seed(2022-04-04)
+data(r_start, package = "spatialising")
+data(r_end, package = "spatialising")
+ts1 = spatial_ising(r_start, B = -0.3, J = 0.7)
+ts10 = spatial_ising(r_start, B = -0.3, J = 0.7, updates = 10)
+ts_oneclass = matrix(rep(1, 9), nrow = 3)
+
+m1 = magnetization(ts1)
+m2 = magnetization(ts10)
+m3 = magnetization(ts_oneclass)
+
+ti1 = texture_index(ts1)
+ti2 = texture_index(ts10)
+ti3 = texture_index(ts_oneclass)
+
+test_that("calculations of metrics works", {
+  expect_equal(length(m1), 1)
+  expect_equal(length(m2), 10)
+  expect_equal(length(m3), 1)
+  expect_true(is.na(ti3))
+})
